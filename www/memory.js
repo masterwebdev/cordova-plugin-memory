cecf77b9c81211649914677692578f79fa1082ed
masterwebdev <masterwebdev@gmail.com>
2017-04-07 23:08:36 +0300

    fix

--- www/memory.js
+++ www/memory.js
@@ -1,10 +1,5 @@
-cordova.define("com.autovitalsinc.memory.memory", function(require, exports, module) {
-/*global cordova, module*/
-
 module.exports = {
     getmemory: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Memory", "getmemory", [name]);
     }
 };
-
-});
